1ab33e95d8d5b952763c6b9297bc04db33aa204e
elias-20 <eliastuiztuy@gmail.com>
2025-03-02 20:59:14 -0600

    Update acceso.h

--- ejerciccio/acceso.h
+++ ejerciccio/acceso.h
@@ -1,11 +1,11 @@
-#ifndef ACCESO_H  
-#define ACCESO_H  
+#ifndef ACCESO_H  //evita la inclucion multiple de un mismo archi
+#define ACCESO_H  //define constante simbolico 
 
-#include "registro.h"
-#include <fstream> 
+#include "registro.h" //incluye en contenido de un archivo 
+#include <fstream> //biblioteca para manejo de archivo 
 
-void leerArchivo();  
-void buscarPorID(int idBuscado);  
+void leerArchivo();  //define unna funcion que se llama leerArchivo
+void buscarPorID(int idBuscado);  //realiza busqueda en una base de datos
 
-#endif 
-#pragma once
+#endif //se utiliza para marcar el final de una condicion de copil
+#pragma once //sirve para indicar un archivo de cabecera 
